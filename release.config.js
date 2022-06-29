bcbb0f1f006d530c4ad4a76d8885273e2588c511
Jez Allan <jez.allan@rvu.co.uk>
2022-06-29 15:27:20 +0100

    chore(release) 2.3.2

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
-    [
-      '@semantic-release/release-notes-generator',
-      { preset: 'conventionalcommits' },
-    ],
-    '@semantic-release/changelog',
+    // [
+    //   '@semantic-release/release-notes-generator',
+    //   { preset: 'conventionalcommits' },
+    // ],
+    // '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
     '@semantic-release/git',
